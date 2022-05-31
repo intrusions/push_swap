bfcd5872736bc131500139dedc3a9e06ceb5852c
Julien CHEVALLIOT <jucheval@e2r3p3.clusters.42paris.fr>
2022-05-31 02:43:57 +0200

    crade

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jucheval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 18:04:11 by jucheval          #+#    #+#             */
-/*   Updated: 2022/05/31 02:15:12 by jucheval         ###   ########.fr       */
+/*   Updated: 2022/05/31 02:42:54 by jucheval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,25 @@ int	ft_prepare_stack(t_data *data, int argc, char **argv, int params)
 	if (argc == 1)
 		return (0);
 	data->len_a = argc - 1;
-	if (!ft_parsing(argv, data->len_a))
+	if (!ft_parsing(argv, data->len_a) && params)
+		return (ft_free_split(argv, argc), 0);
+	else if (!ft_parsing(argv, data->len_a) && !params)
 		return (0);
 	data->stack_a = malloc(sizeof(int) * (data->len_a));
-	if (!data->stack_a)
+	if (!data->stack_a && params)
+		return (ft_free_split(argv, argc), 0);
+	else if (!data->stack_a && !params)
 		return (0);
 	data->stack_b = malloc(sizeof(int) * (data->len_a));
-	if (!data->stack_b)
+	if (!data->stack_b && params)
+		return (ft_free_split(argv, argc), free(data->stack_a), 0);
+	else if (!data->stack_b && !params)
 		return (free(data->stack_a), 0);
 	ft_fill_stack(data->stack_a, data->len_a, argv);
-	if (!ft_check_duplicate_and_max(data->stack_a, data->len_a) && !params)
+	if (params)
+		ft_free_split(argv, argc);
+	if (!ft_check_duplicate_and_max(data->stack_a, data->len_a))
 		return (free(data->stack_a), free(data->stack_b), 0);
-	else if (!ft_check_duplicate_and_max(data->stack_a, data->len_a) && params)
-		return (free(data->stack_a), free(data->stack_b), \
-		ft_free_split(argv, argc), 0);
 	return (1);
 }
 
@@ -50,9 +55,5 @@ int	main(int argc, char **argv)
 	if (!ft_prepare_stack(&data, argc, argv, params))
 		return (ft_error());
 	ft_sort(&data);
-	if (!params)
-		return (free(data.stack_a), free(data.stack_b), 0);
-	else
-		return (ft_free_split(argv, argc), \
-		free(data.stack_a), free(data.stack_b), 0);
+	return (free(data.stack_a), free(data.stack_b), 0);
 }
